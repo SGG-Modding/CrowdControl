33a923ad8d8431c5ca51a1d3859055b829c225c0
Andre Issa <andrelouisissa@gmail.com>
2022-12-28 14:35:09 +1100

    simplified lua side

--- CrowdControlHadesDraft.lua
+++ CrowdControlHadesDraft.lua
@@ -21,9 +21,6 @@ local effectData = {
 
 -- Implementation
 
-local effectsByID = { }
-local effectQueue = { }
-
 local function triggerEffect( id, result )
 	return shared.TriggerEffect( id, result )
 end
@@ -31,34 +28,14 @@ end
 function doAction( id, action )
 	if action( id ) ~= false then
 		triggerEffect( id, "Success" )
-		effectsByID[ id ] = nil
 		return true
 	end
 	return false
 end
 
-local function prepareEffects( )
-	for i = 1, #effectQueue do
-		local id = effectQueue[ i ]
-		local effect = effectsByID[ id ]
-		local data = effectData[ effect ]
-		if data.Trigger( id, data.Action ) then
-			effectQueue[ i ] = nil
-		end
-	end
-	CollapseTable( effectQueue )
-end
-
-local function effectLoop( )
-	while true do
-		wait( 0.25 )
-		prepareEffects( )
-	end
-end
-
 function queueEffect( id, effect )
-	effectsByID[ id ] = effect
-	table.insert( effectQueue, id )
+	local data = effectData[ effect ]
+	data.Trigger( id, data.Action )
 end
 
 local function initShared( )
@@ -74,11 +51,9 @@ end
 -- Internal
 
 CrowdControlHadesDraft.Internal = ModUtil.UpValues( function( )
-	return doAction, actions, triggers, effectsByID, effectData, effectQueue, effectLoop, queueEffect, triggerEffect, prepareEffects, initShared
+	return doAction, actions, triggers, effectData, queueEffect, triggerEffect, initShared
 end )
 
 StyxScribe.AddHook( initShared, "StyxScribeShared: Reset", CrowdControlHadesDraft )
 
 initShared( )
-
-thread( effectLoop )
