4321cf60e2c1cbb83188c882145bc219ee67696f
Andre Issa <andrelouisissa@gmail.com>
2023-01-04 00:36:01 +1100

    CC->python->lua works but needs proper effect name
    lua->python->CC needs to be changed to work fully

--- CrowdControl.py
+++ CrowdControl.py
@@ -1,4 +1,8 @@
 # Depends on StyxScribeShared
+import socket
+import time
+import threading
+import json
 
 __all__ = ["Load", "RequestEffect", "NotifyEffect", "Internal"]
 Internal = {}
@@ -8,11 +12,54 @@ Shared = None
 def NotifyEffect(eid, result):
     # handle the response here
     print(f"Python got response {result} for event with ID {eid}")
+    thread.socket.send(json.dumps({"id":eid, "status":result}).encode('utf-8'))
 
 def RequestEffect(eid, effect):
     return Shared.RequestEffect(eid, effect)
 
+thread = None
+class AppSocketThread(threading.Thread):
+    #repurposed from:
+    # - https://stackoverflow.com/questions/27284358/connect-to-socket-on-localhost
+    # - https://stackoverflow.com/questions/51677868/parse-json-message-from-socket-using-python
+    host = "127.0.0.1"
+    port = 58430
+    socket = None
+
+    def __init__(self, name='cc-app-socket-thread'):
+        global thread
+        super(self.__class__, self).__init__(name=name)
+        thread = self
+        self.start()
+
+    def run(self):
+        print("CrowdControl: Socket Thread Running!")
+        while True:
+            print(f"CrowdControl: Attempting to connect on {self.host}:{self.port}")
+            try:
+                s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                s.connect((self.host,self.port))
+                print(f"CrowdControl: Connected on {self.host}:{self.port}")
+                self.socket = s
+            except ConnectionRefusedError:
+                time.sleep(5)
+                continue
+            while True:
+                message = s.recv(1042)
+                if not message:
+                    continue
+                message = message.decode('utf-8')
+                message = json.loads(message[:-1])
+                eid = message["id"]
+                effect = message["code"]
+                if Shared is None:
+                    print(f"CrowdControl: Need to be loaded into a save to run effects!")
+                    continue
+                RequestEffect(eid, effect)
+
 def Load():
+    #start the app socket thread
+    Scribe.AddOnRun(AppSocketThread, __name__)
 
     def initShared(message):
         global Shared
---
+++ Packs/CrowdControl.Packs.Hades.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using CrowdControl.Common;
+using JetBrains.Annotations;
+using ConnectorType = CrowdControl.Common.ConnectorType;
+
+namespace CrowdControl.Games.Packs
+{
+    [UsedImplicitly]
+    public class Hades : SimpleTCPPack
+    {
+        public override string Host => "127.0.0.1";
+
+        public override ushort Port => 58430;
+
+        public Hades(IPlayer player, Func<CrowdControlBlock, bool> responseHandler, Action<object> statusUpdateHandler) : base(player, responseHandler, statusUpdateHandler) { }
+
+        public override Game Game { get; } = new(5, "Hades", "Hades", "PC", ConnectorType.SimpleTCPConnector);
+
+        public override List<Effect> Effects { get; } = new()
+        {
+            new Effect("Hello World", "hades_examples_helloworld"),
+        };
+    }
+}
