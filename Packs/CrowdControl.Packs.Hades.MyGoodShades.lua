eb1a7f9ca26af4ad2cbdc5c866a8260c224b8077
Phy Poh <phypoh@gmail.com>
2023-01-08 22:31:18 +0800

    Add base SuperMeter effect

---
+++ Packs/CrowdControl.Packs.Hades.MyGoodShades.lua
@@ -0,0 +1,51 @@
+local cc, packs = CrowdControl, CrowdControl.Packs
+local pack = ModUtil.Mod.Register( "MyGoodShades", packs.Hades, false )
+
+pack.Effects = { }; pack.Actions = { }; pack.Triggers = { }
+pack.Parametric = { Actions = { }, Triggers = { } }
+
+do
+	-- Triggers
+	function pack.Parametric.Triggers.DisplayTimer( duration )
+		return function( ... )
+			local duration = duration
+			local args = table.pack( ... )
+			thread( function( )
+				while duration > 1 do
+					ModUtil.Hades.PrintOverhead( duration, 1 )
+					duration = duration - 1
+					wait( 1 )
+				end
+				ModUtil.Hades.PrintOverhead( duration, duration )
+				wait( duration )
+				ModUtil.Hades.PrintOverhead( " ", 0.05 )
+				return cc.InvokeEffect( table.unpack( args ) )
+			end	)
+		end
+	end
+	
+	-- Actions
+	function pack.Actions.BuildSuperMeter()
+		if IsSuperValid() then
+			BuildSuperMeter(CurrentRun, 50)
+			return true
+		end
+		return false
+	end
+
+	function pack.Actions.Suicide( id )
+		return true, KillHero( { }, { }, { } )
+	end
+
+	function pack.Parametric.Actions.PrintStack( ... )
+		return packs.Base.Parametric.Actions.Invoke( ModUtil.Hades.PrintStack, ... )
+	end
+	
+	-- Effects
+	pack.Effects.HelloWorld = pack.Parametric.Actions.PrintStack( "Hello World!" )
+	pack.Effects.TimedSuicide = cc.TimedEffect( 5, pack.Parametric.Triggers.DisplayTimer( 5 ), pack.Actions.Suicide )
+	pack.Effects.BuildSuperMeter = pack.Actions.BuildSuperMeter
+end
+
+-- put our effects into the centralised Effects table, under the "Hades.Examples" path
+ModUtil.Path.Set( "Hades.MyGoodShades", ModUtil.Table.Copy( pack.Effects ), cc.Effects )
